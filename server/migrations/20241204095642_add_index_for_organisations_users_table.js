7a6ef7c75d898af59c3a43aa357c34b585f3f7ad
William Surya Wijaya <williamsw.gb@gmail.com>
2024-12-04 17:00:02 +0700

    add index files to the table project_users, organisations_users, and tasks_assignees

---
+++ server/migrations/20241204095642_add_index_for_organisations_users_table.js
@@ -0,0 +1,19 @@
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.up = function (knex) {
+  return knex.schema.table("organisations_users", function (table) {
+    table.index(["organisation_id", "user_id"]);
+  });
+};
+
+/**
+ * @param { import("knex").Knex } knex
+ * @returns { Promise<void> }
+ */
+exports.down = function (knex) {
+  return knex.schema.table("organisations_users", function (table) {
+    table.dropIndex(["organisation_id", "user_id"]);
+  });
+};
