7c1c45e167380bb3a91fea5f39240cd1b21889cc
William Surya Wijaya <williamsw.gb@gmail.com>
2024-12-04 18:51:54 +0700

    remove old routes, connect route for organisation

--- server/server.js
+++ server/server.js
@@ -1,18 +1,11 @@
-const app = require('./server-config.js');
-const routes = require('./server-routes.js');
+const app = require("./server-config.js");
+const organisationRoutes = require("./routes/organisationRoutes");
 
 const port = process.env.PORT || 5000;
 
-app.get('/', routes.getAllTodos);
-app.get('/:id', routes.getTodo);
+app.use("/organisations", organisationRoutes);
 
-app.post('/', routes.postTodo);
-app.patch('/:id', routes.patchTodo);
-
-app.delete('/', routes.deleteAllTodos);
-app.delete('/:id', routes.deleteTodo);
-
-if (process.env.NODE_ENV !== 'test') {
+if (process.env.NODE_ENV !== "test") {
   app.listen(port, () => console.log(`Listening on port ${port}`));
 }
 
