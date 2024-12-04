9c53b9f0f3129e0818a5c667f9c9516aca88277e
William Surya Wijaya <williamsw.gb@gmail.com>
2024-12-04 20:36:19 +0700

    refactor organisation controller, model, and routes to use base controller and model

--- server/models/organisationModel.js
+++ server/models/organisationModel.js
@@ -1,24 +1,6 @@
 const knex = require("../database/connection");
+const baseModel = require("./baseModel");
 
-const OrganisationModel = {
-  create: (data) => knex("organisations").insert(data).returning("*"),
-  list: ({ limit = 10, offset = 0 }) =>
-    knex("organisations")
-      .where({ deleted_at: null })
-      .limit(limit)
-      .offset(offset),
-  findById: (id) =>
-    knex("organisations").where({ id, deleted_at: null }).first(),
-  update: (id, data) =>
-    knex("organisations")
-      .where({ id, deleted_at: null })
-      .update({ ...data, updated_at: knex.fn.now() })
-      .returning("*"),
-  softDelete: (id) =>
-    knex("organisations")
-      .where({ id, deleted_at: null })
-      .update({ deleted_at: knex.fn.now() })
-      .returning("*"),
-};
+const organisationModel = baseModel("organisations", knex);
 
-module.exports = OrganisationModel;
+module.exports = organisationModel;
--- server/routes/organisationRoutes.js
+++ server/routes/organisationRoutes.js
@@ -1,21 +1,15 @@
 const express = require("express");
-const {
-  createOrganisation,
-  getOrganisation,
-  listOrganisations,
-  updateOrganisation,
-  deleteOrganisation,
-} = require("../controllers/organisationController");
+const organisationController = require("../controllers/organisationController");
 
 const router = express.Router();
 
-router.post("/", createOrganisation); // POST /organisations
+router.post("/", organisationController.create); // POST /organisations
 
-router.get("/", listOrganisations); // GET /organisations
-router.get("/:id", getOrganisation); // GET /organisations/:id
+router.get("/", organisationController.list); // GET /organisations
+router.get("/:id", organisationController.find); // GET /organisations/:id
 
-router.put("/:id", updateOrganisation); // PUT /organisations/:id
+router.put("/:id", organisationController.update); // PUT /organisations/:id
 
-router.delete("/:id", deleteOrganisation); // DELETE /organisations/:id
+router.delete("/:id", organisationController.delete); // DELETE /organisations/:id
 
 module.exports = router;
