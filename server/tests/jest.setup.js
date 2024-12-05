332a74d0d8496c57b26284c126a7c632bac80c5d
William Surya Wijaya <williamsw.gb@gmail.com>
2024-12-05 15:23:38 +0700

    remove migration rollback in jest before all tests run

--- server/tests/jest.setup.js
+++ server/tests/jest.setup.js
@@ -2,7 +2,6 @@ const knex = require("../database/connection");
 
 beforeAll(async () => {
   console.log("Global setup: Setting up the test database...");
-  await knex.migrate.rollback();
   await knex.migrate.latest();
   await knex.seed.run();
 });
