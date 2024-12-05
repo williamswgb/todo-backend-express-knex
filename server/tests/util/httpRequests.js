943664a448459c58696d07362a690f8ffa6bb477
William Surya Wijaya <williamsw.gb@gmail.com>
2024-12-05 23:39:42 +0700

    add put request

--- server/tests/util/httpRequests.js
+++ server/tests/util/httpRequests.js
@@ -1,22 +1,21 @@
-const request = require('supertest');
+const request = require("supertest");
 
-const app = require('../../server.js');
-
+const app = require("../../server.js");
 
 // a helper function to make a POST request.
 function post(url, body) {
   const httpRequest = request(app).post(url);
   httpRequest.send(body);
-  httpRequest.set('Accept', 'application/json')
-  httpRequest.set('Origin', 'http://localhost:5000')
+  httpRequest.set("Accept", "application/json");
+  httpRequest.set("Origin", "http://localhost:5000");
   return httpRequest;
 }
 
 // a helper function to make a GET request.
 function get(url) {
   const httpRequest = request(app).get(url);
-  httpRequest.set('Accept', 'application/json')
-  httpRequest.set('Origin', 'http://localhost:5000')
+  httpRequest.set("Accept", "application/json");
+  httpRequest.set("Origin", "http://localhost:5000");
   return httpRequest;
 }
 
@@ -24,24 +23,33 @@ function get(url) {
 function patch(url, body) {
   const httpRequest = request(app).patch(url);
   httpRequest.send(body);
-  httpRequest.set('Accept', 'application/json')
-  httpRequest.set('Origin', 'http://localhost:5000')
+  httpRequest.set("Accept", "application/json");
+  httpRequest.set("Origin", "http://localhost:5000");
+  return httpRequest;
+}
+
+// a helper function to make a PUT request.
+function put(url, body) {
+  const httpRequest = request(app).put(url);
+  httpRequest.send(body);
+  httpRequest.set("Accept", "application/json");
+  httpRequest.set("Origin", "http://localhost:5000");
   return httpRequest;
 }
 
 // a helper function to make a DELETE request.
 function del(url) {
   const httpRequest = request(app).delete(url);
-  httpRequest.set('Accept', 'application/json')
-  httpRequest.set('Origin', 'http://localhost:5000')
+  httpRequest.set("Accept", "application/json");
+  httpRequest.set("Origin", "http://localhost:5000");
   return httpRequest;
 }
-
 
 module.exports = {
   post,
   get,
   patch,
+  put,
   del,
-  delete: del
+  delete: del,
 };
